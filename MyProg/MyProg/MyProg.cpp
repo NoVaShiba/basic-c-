282bfca50798cdcc3c97c03e34bb39250c030306
  <v.shibalkovv.@mail.ru>
2017-03-30 23:11:33 +0400

    First

---
+++ MyProg/MyProg/MyProg.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+	char sym;
+
+	x = 5;
+	y = 10;
+	sym = '*';
+
+	cout << "Эта программа выводит занчение переменной x, y, sym: ";
+	cout << x, y, sym;
+
+	return 0;
+}
