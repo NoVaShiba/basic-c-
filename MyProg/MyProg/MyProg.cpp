f24197d69df4df1951d48328c7b89475b6b83f2f
  <v.shibalkovv.@mail.ru>
2017-03-31 20:42:08 +0400

    Это очень простой блок!

--- MyProg/MyProg/MyProg.cpp
+++ MyProg/MyProg/MyProg.cpp
@@ -7,10 +7,23 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	int count;
+	int a, b;
 
-	for (count = 1; count <= 100; count = count + 1)
-		cout << count << " ";
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+
+	if (a < b) {
+		cout << "Первое чило меньше второго.\n";
+		cout << "Их разность равна: .\n" << b - a;
+		cout << "Их сумма равна: .\n" << b + a;
+	}
+	else if (a > b) {
+		cout << "Первое число больше второго.\n";
+		cout << "Их разность равна: .\n" << a - b;
+		cout << "Их сумма равна: .\n" << a + b;
+	}
 
 
 	return 0;
