1306d2b1abd9770ef0d494cbd57d919d23c92ef1
  <v.shibalkovv.@mail.ru>
2017-03-31 13:51:31 +0400

    Это пример создания прототипа функции

--- MyProg/MyProg/MyProg.cpp
+++ MyProg/MyProg/MyProg.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <locale>
 using namespace std;
+
+void myfunc();
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	double gallons, liters;
 
-	cout << "Введите количество галлонов: ";
-	cin >> gallons;
-
-	liters = gallons * 3.7854;
-
-	cout << "Литров: " << liters;
+	cout << "В функции main().";
+	myfunc();
+	cout << "Снова в функции main().";
 
 	return 0;
+}
+
+void myfunc()
+{
+	cout << "В функции myfunc().";
 }
