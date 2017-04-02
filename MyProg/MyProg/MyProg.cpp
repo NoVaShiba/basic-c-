29b0a6ba161377ed68b708921b01b079a7c1b06e
  <v.shibalkovv.@mail.ru>
2017-04-02 11:54:08 +0400

    Так выглядит иницилизация переменых.

--- MyProg/MyProg/MyProg.cpp
+++ MyProg/MyProg/MyProg.cpp
@@ -3,26 +3,30 @@
 #include <locale>
 using namespace std;
 
-void func();
+void total(int x);
+
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	int x;
+	cout << "Вычисление суммы числел от 1 до 5.\n";
+	total(5);
 
-	x = 10;
-	func();
-	cout << "\n";
-	cout << x;
-
+	cout << "\nВычисление суммы чисел от 1 до 6 .\n";
+	total(6);
 
 	return 0;
 }
 
-void func() {
-	int x;
+void total(int x)
+{
+	int sum = 0;
+	int i, count;
 
-	x = -199;
-	cout << x;
+	for (i = 1; i <= x; i++) {
+		sum = sum + i;
+		for (count = 0; count < 10; count++) cout << '.';
+		cout << "Промежуточная сумма равна " << sum << '\n';
+	}
 }
