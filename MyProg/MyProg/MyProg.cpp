4ee4da0303d290c5fd4442bf0486261cdb2977db
  <v.shibalkovv.@mail.ru>
2017-04-03 19:52:30 +0400

    Это моя первая более мения проработанная игра.

--- MyProg/MyProg/MyProg.cpp
+++ MyProg/MyProg/MyProg.cpp
@@ -3,31 +3,59 @@
 #include <locale>
 using namespace std;
 
-void total(int x);
-
+void play(int m);
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	cout << "Вычисление суммы числел от 1 до 5.\n";
-	total(5);
+	int option;
+	int magic; 
 
-	cout << "\nВычисление суммы чисел от 1 до 6 .\n";
-	total(6);
+	magic = rand();
 
+	do {
+		cout << "1. Получить новое магическое число\n";
+		cout << "2. Сыграть\n";
+		cout << "3. Выйти из программы\n";
+		do {
+			cout << "Введте свой вариант: ";
+			cin >> option;
+		} while (option < 1 || option > 3);
+		switch (option) {
+		case 1:
+			magic = rand();
+			break;
+		case 2:
+			play(magic);
+			break;
+		case 3:
+			cout << "До свидания!\n";
+			break;
+		}
+	} while (option != 3);
+	
 	system("pause");
 	return 0;
 }
 
-void total(int x)
+void play(int m)
 {
-	int sum = 0;
-	int i, count;
+	int t, x;
 
-	for (i = 1; i <= x; i++) {
-		sum = sum + i;
-		for (count = 0; count < 10; count++) cout << '.';
-		cout << "Промежуточная сумма равна " << sum << '\n';
+	for (t = 0; t < 100; t++) {
+		cout << "Угадайте магическое число: ";
+		cin >> x;
+
+		if (x == m) {
+			cout << "** Правильно**\n";
+			return;
+		}
+		else {
+			if (x < m) cout << "Магическое число больше.\n";
+			else cout << "Магическое число меньше.\n";
+		}
 	}
+	cout << "Вы использовали все шансы угадать число. "
+		<< "Попытайтесь снова.\n";
 }
