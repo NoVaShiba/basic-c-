29a94a733844d5a406773b498a735728725d0489
  <v.shibalkovv.@mail.ru>
2017-04-21 18:50:15 +0400

    Это пример перечисления Enum.

--- MyProg/MyProg/MyProg.cpp
+++ MyProg/MyProg/MyProg.cpp
@@ -3,48 +3,38 @@
 #include <locale>
 using namespace std;
 
-void drill();
-
-int coun;  //Переменные cout и num_right -- глобальные.
-int num_right;
+enum mall {parking = -1, hall, many_shops, roof};
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
-	cout << "Сколько практических упражнений: ";
-	cin >> coun;
+	int floor;
+	bool exit = false;
 
-	num_right = 0;
-	do {
-		drill();
-		coun--;
-	} while (coun);
-	cout << " Вы дали " << num_right << " правильных ответов.\n";
+	while (!exit) {
+		cout << "Enter your floor (from -1 to 2): " << endl;
+		cin >> floor;
+		switch (floor) {
+			case parking:
+				cout << "You on the -1 floor - parking" << endl;
+				break;
+			case hall:
+				cout << "You on the 0 floor - hall" << endl;
+				break;
+			case many_shops:
+				cout << "You on the 1 floor - many shops" << endl;
+				break;
+			case roof:
+				cout << "You on the 2 floor - roof" << endl;
+				break;
+			default:
+				cout << "Floors only from -1 to 2" << endl;
+		}
+		cout << "If yo want play again press 0 or press 1 to exit this game" << endl;
+		cin >> exit;
+	}
 
 	system("pause");
 	return 0;
-}
-
-void drill()
-{
-	int count; //Эта переменная count -- локальна и никак не связанна с одноименной глобальной.
-	int a, b, ans;
-
-	//Генерируем числа от 0 до 99.
-	a = rand() % 100;
-	b = rand() % 100;
-
-	//Пользователь получает три попытки дать правильный ответ
-	for (count = 0; count < 3; count++) {
-		cout << "\nСколько будет " << a << " + " << b << "?" << '\n';
-		cin >> ans;
-		if (ans == a + b) {
-			cout << "Правильно.";
-			num_right++;
-			return;
-		}
-	}
-	cout << "Вы использовали все попытки.\n";
-	cout << "Ответ равен " << a + b << '\n';
 }
